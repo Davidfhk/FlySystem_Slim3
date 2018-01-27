f2a0fa1b88b2883497ed0e8c8b03bdd103bc65c5
Davidfhk <davidign1688@hotmail.com>
2018-01-27 17:22:48 +0100

    Tabulaciones, y orden en las dependencias

--- App/Dependencies.php
+++ App/Dependencies.php
@@ -5,30 +5,50 @@
  * DEPENDENCY CONTAINER: $container
  */
 $container['ServiceSettings'] = require __DIR__ . "/flysystemws/config/dev/settings.php";
-$container['FTP'] = function($c){
-    return new League\Flysystem\Filesystem($c['AdapterFTP']);
-    };
-    
-$container['Manager'] = function($c){
-    return new League\Flysystem\MountManager(['ftp' => $c['FTP'],'local' => $c['Local']]);
-    };
-    
- $container['Local'] = function($c){
-    return new League\Flysystem\Filesystem($c['AdapterLocal']);
- };
- 
- $container['AdapterLocal'] = function($c){
-    return new League\Flysystem\Adapter\Local(__DIR__);
- };
 
-  $container['AdapterFTP'] = function($c){
-    return new League\Flysystem\Adapter\Ftp($c['ServiceSettings']['ftp']);
- };
  
- $container['FlysystemModel']=function($c){
-    return new App\flysystemws\FlysystemModel($c['Manager']);
- };
+$container['FlysystemController']=function($c){
+    return new App\flysystemws\FlysystemController(
+        $c['FlysystemModel']
+    );
+};
  
-  $container['FlysystemController']=function($c){
-    return new App\flysystemws\FlysystemController($c['FlysystemModel']);
- };
+$container['FlysystemModel']=function($c){
+    return new App\flysystemws\FlysystemModel(
+        $c['Manager']
+    );
+};
+
+$container['Manager'] = function($c){
+
+    return new League\Flysystem\MountManager(
+        [
+            'ftp' => $c['Ftp'],
+            'local' => $c['Local']
+        ]
+    );
+};
+
+$container['Ftp'] = function($c){
+    return new League\Flysystem\Filesystem(
+        $c['AdapterFtp']
+    );
+};
+    
+$container['Local'] = function($c){
+
+    return new League\Flysystem\Filesystem(
+        $c['AdapterLocal']
+    );
+};
+ 
+$container['AdapterLocal'] = function($c){
+    return new League\Flysystem\Adapter\Local(__DIR__);
+};
+
+$container['AdapterFtp'] = function($c){
+
+    return new League\Flysystem\Adapter\Ftp(
+        $c['ServiceSettings']['ftp']
+    );
+};
