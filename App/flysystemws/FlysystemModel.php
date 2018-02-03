5c9e3ec7c3ec6a98ff16119b6db89462849758d4
Davidfhk <davidign1688@hotmail.com>
2018-02-03 21:46:40 +0100

    Correcciones y añadimos el metodo deleteFileFtp, el cual se encargara de eliminar el fichero de FTP

--- App/flysystemws/FlysystemModel.php
+++ App/flysystemws/FlysystemModel.php
@@ -31,23 +31,34 @@ class FlysystemModel
         $this->manager = $manager;
     }
     
-    public function getFile($fileFtp){
+    public function getFile($fileFtp)
+    {
 
         $this->contents= $this->manager->read('ftp://'.$fileFtp);
     }
     
-    public function writeLocalFile($fileLocal){
+    public function writeLocalFile($fileLocal)
+    {
         
         $this->manager->write('local://'.$fileLocal, $this->contents);
 
         echo "El contenido se te ha escrito con exito en el archivo ".$fileLocal;
     }
 
-    public function updateContent($fileFtp,$newContent){
+    public function updateContent($fileFtp,$newContent)
+    {
 
         $this->manager->update('ftp://'.$fileFtp,$newContent);
 
-        echo "El contenido se ha actualizado con exito por ".$newContent;
+        echo "El contenido se ha actualizado con exito por el siguiente texto: ".$newContent;
+    }
+
+    public function deleteFileFtp($fileFtp)
+    {
+
+        $this->manager->delete('ftp://'.$fileFtp);
+
+        echo "El fichero ".$fileFtp." ha sido eliminado con exito";
     }
 
 }
