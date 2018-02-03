29e05aeef8526e673ae40c99afc591acb19d1a7d
Davidfhk <davidign1688@hotmail.com>
2018-02-03 21:48:09 +0100

    Añadimos la ruta delete, la cual se encargara de eliminar el fichero de FTP

--- App/Routes.php
+++ App/Routes.php
@@ -4,5 +4,6 @@ $app->group('/flysystem', function(){
 
 	$this->post('','FlysystemController:readWrite');
 	$this->put('','FlysystemController:updateContent');
+	$this->delete('','FlysystemController:deleteFileFtp');
 })->add('CheckFileMw');
 
